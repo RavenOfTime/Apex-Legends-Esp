71ee5307d3671e470775167eb631ebb3aa70aaf1
Sk Md Asib <98967932+Real-Ascarre@users.noreply.github.com>
2022-02-16 21:27:25 +0530

    Season 12 Updated

--- Apex Legends/UserMode/Offsets.h
+++ Apex Legends/UserMode/Offsets.h
@@ -2,22 +2,22 @@
 
 namespace Offsets {
 	enum Offsets {
-		EntityList = 0x1972EA8,
+		EntityList = 0x1973AD8,//Season 12 Updated on 16/02/2002
 
-		ViewMatrix = 0x1D504D0,
+		ViewMatrix = 0x10E8D0,//Season 12 Updated on 16/02/2002
 
-		LocalPlayer = 0x1D22708,
+		LocalPlayer = 0x1D233D8,//Season 12 Updated on 16/02/2002
 
 		TeamId = 0x448,
 
-		Health = 0x438,
-		MaxHealth = 0x578,
+		Health = 0x438,//Season 12 Updated on 16/02/2002
+		MaxHealth = 0x578,//Season 12 Updated on 16/02/2002
 
-		Origin = 0x14C,
+		Origin = 0x14C,//Season 12 Updated on 16/02/2002
 
-		Knocked = 0x2718,
-		Dead = 0x798,
+		Knocked = 0x2718,//Season 11 Need Update of doesnt Work
+		Dead = 0x798,//Season 11 Need Update of doesnt Work
 
-		BoneArray = 0xF38,
+		BoneArray = 0xF38,//Season 12 Updated on 16/02/2002
 	};
 }
