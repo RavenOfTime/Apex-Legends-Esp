9ef489dc954596bdece64bba17ed20bc3e42af04
Sk Md Asib <98967932+Real-Ascarre@users.noreply.github.com>
2022-02-16 20:56:56 +0530

    Driver Updated

---
+++ Driver/Main.cpp
@@ -0,0 +1,10 @@
+#include "Hook.h"
+
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING reg_path) {
+	UNREFERENCED_PARAMETER(driver_object);
+	UNREFERENCED_PARAMETER(reg_path);
+
+	call_kernel_function(&hook_handler);
+
+	return STATUS_SUCCESS;
+}
